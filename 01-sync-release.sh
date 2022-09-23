6ccc9719b2da0e871b9a987a008cc346a554fb15
Kan-Ru Chen <kanru@kanru.info>
2022-09-24 01:35:10 +0900

    feat: check released version

--- 01-sync-release.sh
+++ 01-sync-release.sh
@@ -2,7 +2,21 @@
 
 set -x
 
-VERSION=$1
+FLATHUB_VERSION=$(curl 'https://release-monitoring.org/api/v2/packages/?name=com.logseq.Logseq&distribution=Flathub'|jq -r '.items[].version')
+LATEST_VERSION=$(curl 'https://release-monitoring.org/api/v2/versions/?project_id=291486'|jq -r .latest_version)
+FORCE_VERSION=$1
+
+if test -z "$FORCE_VERSION"; then
+    VERSION=$LATEST_VERSION
+    if test $FLATHUB_VERSION = $VERSION; then
+        echo "Already up to date."
+        exit
+    fi
+else
+    VERSION=$FORCE_VERSION
+fi
+
+
 BASE_URL=https://github.com/logseq/logseq/archive/refs/tags
 
 curl -L -o logseq-$VERSION.tar.gz $BASE_URL/$VERSION.tar.gz
